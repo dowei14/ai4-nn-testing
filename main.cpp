f16cc00005df6de7f1ee80d1ad73d82487bfb72a
Dominik Weickgenannt <dowei14@student.sdu.dk>
2015-11-17 18:18:25 +0100

    loading input and basic setup done

--- main.cpp
+++ main.cpp
@@ -1,74 +1,106 @@
-/*****************************************************************************
- *  Copyright (C) 2012 by Timo Nachstedt                                     *
- *                                                                           *
- *  This program is free software: you can redistribute it and/or modify     *
- *  it under the terms of the GNU General Public License as published by     *
- *  the Free Software Foundation, either version 3 of the License, or        *
- *  (at your option) any later version.                                      *
- *                                                                           *
- *  This program is distributed in the hope that it will be useful,          *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
- *  GNU General Public License for more details.                             *
- *                                                                           *
- *  You should have received a copy of the GNU General Public License        *
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
- *                                                                           *
- ****************************************************************************/
-
 #include <utils/ann-framework/ann.h>
 #include <utils/ann-framework/backpropagation.h>
 #include <utils/ann-framework/neuron.h>
 #include <iostream>
-#include <map>
+#include <fstream>
+#include <random>
+#include <sstream>
+#include <string>
 
-/**
- * "Batch mode learning"
- * Our feed-forward neural network for testing
- *
- *  Neurons 0,1,5 are input neurons
- *  Neurons 2,3 are hidden neurons
- *  Neuron 4 is the output neuron
- *
- *  Neuron 5 will be used as bias neuron with fixed input.
- *
- */
-class TestANN : public ANN
+using namespace std;
+
+class CSMTL : public ANN
 {
-  public:
-    TestANN();
+	public:
+		CSMTL();
+		enum { nn_inputs = 15, nn_hidden = 20, nn_outputs=2 };
+		
 };
 
-TestANN::TestANN() {
+CSMTL::CSMTL() {
 
-  //set transfer function
-  //setDefaultTransferFunction(ANN::identityFunction());
-  //setDefaultTransferFunction(ANN::tanhFunction());
-  setDefaultTransferFunction(ANN::logisticFunction());
-  //setDefaultTransferFunction(ANN::thresholdFunction());
+	//set transfer function
+//	setDefaultTransferFunction(ANN::tanhFunction());
+	setDefaultTransferFunction(ANN::logisticFunction());
 
-  setNeuronNumber(5); // total number of neurons
+	setNeuronNumber(nn_inputs+nn_hidden+nn_outputs); // total number of neurons
 
-// Set transfer function for individual neuron
-//  n(0)->setTransferFunction(ANN::logisticFunction()); // input neuron
-//  n(1)->setTransferFunction(ANN::identityFunction()); // input neuron
-//  n(2)->setTransferFunction(ANN::logisticFunction()); // hidden neuron
-//  n(3)->setTransferFunction(ANN::tanhFunction()); // hidden neuron
-//  n(4)->setTransferFunction(ANN::logisticFunction()); // output neuron
+	// create random number generator between -1 and 1
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 eng(rd()); // seed the generator
+    std::uniform_real_distribution<> distr(-1, 1); // define the range
 
-  w(2,0, 0.1); // synapse from neuron 0 to 2 with weight 0.1
-  w(2,1, 0.8);
-
-  w(3,0,-1.0);
-  w(3,1, 1.0);
-
-  w(4,2, 0.1);
-  w(4,3,-0.7);
+	// initialize weights
+	for (int h=nn_inputs; h<(nn_inputs+nn_hidden);h++){
+		for (int i=0; i<nn_inputs;i++){
+			w(h,i,distr(eng));
+		}
+	}
+	for (int o=nn_inputs+nn_hidden; o<(nn_inputs+nn_hidden+nn_outputs);o++){
+		for (int h=nn_inputs; h<(nn_inputs+nn_hidden);h++){
+			w(o,h,distr(eng));
+		}
+	}
 }
 
 int main(int argc, char **argv) {
-  TestANN ann;
+	CSMTL ann;
 
+	// create a topolocial sorting of our network. This is required for the
+	// backpropagation algorithm as "Full Batch mode"
+	ann.updateTopologicalSort();
+
+	// create backpropagation object
+	Backpropagation trainer;
+	trainer.setNeuralNetwork(&ann);
+	for (int i=0; i<ann.nn_inputs;i++){
+		trainer.defineInputNeuron(i, ann.getNeuron(i));
+	}
+	for (int o=ann.nn_inputs+ann.nn_hidden; o<(ann.nn_inputs+ann.nn_hidden+ann.nn_outputs);o++){
+		trainer.defineOutputNeuron(o, ann.getNeuron(o));
+	}	
+	trainer.includeAllSynapses();
+	trainer.includeAllNeuronBiases();
+	trainer.setLearningRate(0.01);
+	
+	
+	ifstream data_inputs("../data/inCSMTL1.txt"); //opening an input stream for file test.txt
+	ifstream data_outputs("../data/outCSMTL1.txt"); //opening an input stream for file test.txt
+	std::string line_out;
+	for(std::string line_in; std::getline(data_inputs, line_in); )   //read stream line by line
+	{
+		// create Training pattern
+		TrainingPattern* p = new TrainingPattern;
+		
+		// add inputs
+		std::istringstream in(line_in);      //make a stream for the line itself
+		//cout<<"Inputs: ";
+		for (int i=0;i<ann.nn_inputs;i++){
+			double x;
+			in >> x;                  //and read the first whitespace-separated token
+			p->inputs[i] = x;
+			//cout<<x<<" ";
+		}
+
+		// add outputs
+		std::getline(data_outputs, line_out);
+		std::istringstream out(line_out);
+		//cout<<" Outputs: ";
+		for (int o=0;o<ann.nn_outputs;o++){
+			double x;
+			out >> x;                  //and read the first whitespace-separated token
+			p->outputs[o] = x;
+			//cout<<x<<" ";
+		}
+		// add pattern to trainer
+		trainer.addTrainingPattern(p);
+		//cout<<endl;
+	}
+	trainer.learn(5);
+	std::cout << ann.dumpWeights();
+
+
+/*
   // create a topolocial sorting of our network. This is required for the
   // backpropagation algorithm as "Full Batch mode"
   ann.updateTopologicalSort();
@@ -120,5 +152,6 @@ int main(int argc, char **argv) {
   std::cout << "1 1 => " << ann.getOutput(4) << std::endl;
 
   std::cout << ann.dumpWeights();
+  */
 }
 
