da48dd17db70f718f4e86620d262054a431ee145
Dominik Weickgenannt <dowei14@student.sdu.dk>
2015-12-05 10:55:48 +0100

    current status

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <string>
 
 using namespace std;
-
+/*
 class CSMTL : public ANN
 {
 	public:
@@ -42,63 +42,202 @@ CSMTL::CSMTL() {
 		}
 	}
 }
-
-int main(int argc, char **argv) {
-	CSMTL ann;
-
-	// create a topolocial sorting of our network. This is required for the
-	// backpropagation algorithm as "Full Batch mode"
-	ann.updateTopologicalSort();
-
-	// create backpropagation object
-	Backpropagation trainer;
-	trainer.setNeuralNetwork(&ann);
-	for (int i=0; i<ann.nn_inputs;i++){
-		trainer.defineInputNeuron(i, ann.getNeuron(i));
-	}
-	for (int o=ann.nn_inputs+ann.nn_hidden; o<(ann.nn_inputs+ann.nn_hidden+ann.nn_outputs);o++){
-		trainer.defineOutputNeuron(o, ann.getNeuron(o));
-	}	
-	trainer.includeAllSynapses();
-	trainer.includeAllNeuronBiases();
-	trainer.setLearningRate(0.01);
-	
-	
-	ifstream data_inputs("../data/inCSMTL1.txt"); //opening an input stream for file test.txt
-	ifstream data_outputs("../data/outCSMTL1.txt"); //opening an input stream for file test.txt
-	std::string line_out;
-	for(std::string line_in; std::getline(data_inputs, line_in); )   //read stream line by line
-	{
-		// create Training pattern
-		TrainingPattern* p = new TrainingPattern;
+*/
+class StateNN : public ANN
+{
+	public:
+		StateNN();
+		enum { nn_inputs = 11, nn_hidden = 20, nn_outputs=7 };
 		
+};
+
+StateNN::StateNN() {
+
+	//set transfer function
+	setDefaultTransferFunction(ANN::tanhFunction());
+//	setDefaultTransferFunction(ANN::logisticFunction());
+
+	setNeuronNumber(nn_inputs+nn_hidden+nn_outputs); // total number of neurons
+
+	// create random number generator between -1 and 1
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 eng(rd()); // seed the generator
+    std::uniform_real_distribution<> distr(-1, 1); // define the range
+
+	// initialize weights
+	for (int h=nn_inputs; h<(nn_inputs+nn_hidden);h++){
+		for (int i=0; i<nn_inputs;i++){
+			w(h,i,distr(eng));
+		}
+	}
+	for (int o=nn_inputs+nn_hidden; o<(nn_inputs+nn_hidden+nn_outputs);o++){
+		for (int h=nn_inputs; h<(nn_inputs+nn_hidden);h++){
+			w(o,h,distr(eng));
+		}
+	}
+}
+
+	double train_in[60000][18];
+	double train_out[60000][7];
+
+	double test_in[60000][18];
+	double test_out[60000][7];
+
+void loadData(int inputs){
+	ifstream data_inputs("../data/train/inRNN_11.txt"); //opening an input stream for file test.txt
+	ifstream data_outputs("../data/train/outRNN.txt"); //opening an input stream for file test.txt
+	std::string line_out;
+
+	long trainer_counter = 0;
+	for(std::string line_in; std::getline(data_inputs, line_in); )   //read stream line by line
+	{		
 		// add inputs
 		std::istringstream in(line_in);      //make a stream for the line itself
 		//cout<<"Inputs: ";
-		for (int i=0;i<ann.nn_inputs;i++){
-			double x;
-			in >> x;                  //and read the first whitespace-separated token
-			p->inputs[i] = x;
-			//cout<<x<<" ";
+		for (int i=0;i<inputs;i++){
+			in >> train_in[trainer_counter][i];                  //and read the first whitespace-separated token			
+			//cout<<train_in[trainer_counter][i]<<" ";
 		}
 
 		// add outputs
 		std::getline(data_outputs, line_out);
 		std::istringstream out(line_out);
 		//cout<<" Outputs: ";
-		for (int o=0;o<ann.nn_outputs;o++){
-			double x;
-			out >> x;                  //and read the first whitespace-separated token
-			p->outputs[o] = x;
-			//cout<<x<<" ";
+		for (int o=0;o<7;o++){
+			out >> train_out[trainer_counter][o];                  //and read the first whitespace-separated token		
+			//cout<<train_out[trainer_counter][o]<<" ";
 		}
-		// add pattern to trainer
-		trainer.addTrainingPattern(p);
+		
 		//cout<<endl;
+		trainer_counter++;
 	}
-	trainer.learn(5);
-	std::cout << ann.dumpWeights();
 
+	ifstream data_inputs2("../data/test/inRNN_11.txt"); //opening an input stream for file test.txt
+	ifstream data_outputs2("../data/test/outRNN.txt"); //opening an input stream for file test.txt
+	long test_counter = 0;
+	for(std::string line_in; std::getline(data_inputs2, line_in); )   //read stream line by line
+	{
+		
+		// add inputs
+		std::istringstream in(line_in);      //make a stream for the line itself
+		//cout<<"Inputs: ";
+		for (int i=0;i<inputs;i++){
+			in >> test_in[test_counter][i];                  //and read the first whitespace-separated token
+			
+		}
+		
+		std::getline(data_outputs2, line_out);
+		std::istringstream out(line_out);
+
+		for (int o=0; o<7;o++){
+			out >> test_out[test_counter][o];
+		}
+		test_counter++;
+	}
+	/*
+	cout<<"Train"<<endl;
+	for (int i=0;i<18;i++) std::cout<<train_in[0][i]<<" ";
+	cout<<endl;
+	for (int i=0;i<7;i++) std::cout<<train_out[0][i]<<" ";
+	cout<<endl;
+	cout<<"Test"<<endl;
+	for (int i=0;i<18;i++) std::cout<<test_in[0][i]<<" ";
+	cout<<endl;
+	for (int i=0;i<7;i++) std::cout<<test_out[0][i]<<" ";
+	cout<<endl;
+	*/
+}
+
+int main(int argc, char **argv) {
+	int input_size = 11;
+	loadData(input_size);
+
+	ofstream saveFile1;
+	saveFile1.open("result.txt",ios::out);
+	saveFile1.precision(5);
+	saveFile1<<fixed;
+
+
+
+for (int q=0;q<20;q++){
+	cout<<"run: "<<q<<endl;
+	StateNN ann;
+	//cout<<ann.dumpWeights()<<endl;
+	// create a topolocial sorting of our network. This is required for the
+	// backpropagation algorithm as "Full Batch mode"
+	ann.updateTopologicalSort();
+	for (int a=0;a<100;a++){
+		// create backpropagation object
+		Backpropagation trainer;
+		trainer.setNeuralNetwork(&ann);
+		for (int i=0; i<ann.nn_inputs;i++){
+			trainer.defineInputNeuron(i, ann.getNeuron(i));
+		}
+		int output_counter = 0;
+		for (int o=ann.nn_inputs+ann.nn_hidden; o<(ann.nn_inputs+ann.nn_hidden+ann.nn_outputs);o++){
+			trainer.defineOutputNeuron(output_counter, ann.getNeuron(o));
+			output_counter++;
+		}	
+		trainer.includeAllSynapses();
+		trainer.includeAllNeuronBiases();
+		trainer.setLearningRate(0.01);
+		// create mini_batch	
+		int mini_batch = 100;
+		for (int i=0;i<mini_batch;i++){
+			TrainingPattern* p = new TrainingPattern;
+			std::random_device rd; // obtain a random number from hardware
+	   	 	std::mt19937 eng(rd()); // seed the generator
+			std::uniform_real_distribution<> distr(0, 33475); // define the range
+			int run_id =(int)distr(eng);
+			//cout<<run_id<<" ";
+			for (int n=0;n<input_size;n++){
+				p->inputs[n] = train_in[run_id][n];
+			}
+			for (int n=0;n<7;n++){
+				p->outputs[n] = train_out[run_id][n];
+			}
+			trainer.addTrainingPattern(p);
+		}
+		//cout<<endl;
+
+		// train
+		trainer.learn(10);
+
+		// test
+		long errors = 0;
+		for (long n=0;n<55007;n++){		
+			for (int i=0;i<input_size;i++){
+				ann.setInput(i,test_in[n][i]);
+			}
+			ann.feedForwardStep();
+			double max = -9999;
+			int maxID = 0;
+			for (int i=0;i<7;i++){
+				double val = ann.getOutput(i);
+				if (val > max) {
+					max = val;
+					maxID = i;
+				}
+			}
+			bool error = false;
+			for (int i=0;i<7;i++){
+				double val;
+				if (i == maxID) val = 1.0;
+				else val = 0.0;
+				if (val != test_out[n][i]) error = true;
+				//saveFile1 << val<<" ";
+			}
+			//saveFile1<<"\n";
+			if (error) errors++;
+		}
+		cout<<(double)errors/55007<<endl;
+		saveFile1 << (a+1)*10<< " " <<(double)errors/55007<<"\n";
+		//cout<<ann.dumpWeights()<<endl;
+	}
+}
+//	
+//}
+	
 
 /*
   // create a topolocial sorting of our network. This is required for the
